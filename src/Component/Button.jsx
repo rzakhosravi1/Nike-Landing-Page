5b0358dd4fe3a22b735551bc2e2fea122de681bc
RezaKhosravi <reza.;hosravi.dev@gmail.com>
2026-01-29 18:53:16 +0330

    make Subscribe Section

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -1,20 +1,22 @@
-const Button = ({ text, iconUrl, color, textColor }) => {
+const Button = ({ text, iconUrl, color, textColor, widthFull }) => {
   return (
-    <div>
-      <button
-        className={`${textColor ? `${textColor} border` : "text-white"} font-montserrat text-md leading-7 px-7 py-3 rounded-full flex items-center
-       ${color} gap-x-2 cursor-pointer `}
-      >
-        {text}
-        {iconUrl && (
-          <img
-            alt="arrow right icon"
-            className="ml-2 rounded-full bg-white w-5 h-5"
-            src={iconUrl}
-          />
-        )}
-      </button>
-    </div>
+    <button
+      className={`
+        flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+        ${textColor ? textColor : "text-white border-coral-red"} 
+        ${color ? color : "bg-coral-red"} 
+        ${widthFull && "w-full"}
+      `}
+    >
+      {text}
+      {iconUrl && (
+        <img
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+          src={iconUrl}
+        />
+      )}
+    </button>
   );
 };
 
