ae58b845168e5e5be00b8d04db780bf32481662c
RezaKhosravi <reza.;hosravi.dev@gmail.com>
2026-02-04 14:04:26 +0330

    fixed hero section bug

--- src/Section/Hero.jsx
+++ src/Section/Hero.jsx
@@ -37,7 +37,11 @@ const Hero = () => {
           ))}
         </div>
       </div>
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+      <div
+        className="relative flex-1 flex justify-center items-center  
+      max-xl:rounded-xl
+      xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center"
+      >
         <img
           src={bigShoe}
           alt="ShoeLogo"
